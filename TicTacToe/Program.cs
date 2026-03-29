19cb3bd82205f86ef4f349997d9cc5962d247505
Zefirka <lizakurbanova68744@gmail.com>
2026-03-29 15:50:20 +0800

    Этап 1: Добавлен label с ФИО автора

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿// Автор: Курбанова Елизавета Валиержоновна
+// Группа: ИСП(9)-23-1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
